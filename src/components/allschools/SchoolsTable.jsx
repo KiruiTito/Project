13d3052d2f063e6989a416a606e8e5e15de35337
Georgeches <georgechesire83@gmail.com>
2023-09-30 15:34:00 +0300

    create and designs table. add search functionality

--- src/components/allschools/SchoolsTable.jsx
+++ src/components/allschools/SchoolsTable.jsx
@@ -1,31 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import './schools.css'
 
 const SchoolsTable = ({ schoolsData }) => {
+
+  const [searchName, setNameSearch] = useState("")
+  const [searchLocation, setLocationSearch] = useState("")
+
+  const filteredSchoolData = schoolsData.filter(data=>data.name.toLowerCase().includes(searchName.toLowerCase())).filter(filteredData=>filteredData.location.toLowerCase().includes(searchLocation.toLowerCase()))
+
   return (
-    <div className="table-container">
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Location</th>
-            <th>Sublocation</th>
-            <th>Address</th>
-          </tr>
-        </thead>
-        <tbody>
-          {schoolsData.map((school) => (
-            <tr key={school.id}>
-              <td>{school.id}</td>
+    <>
+      <div className="p-3">
+        <div className="table-search ps-2 mb-4">
+          <div className="row">
+          <input type='text' placeholder="Filter using name of school" className="form-control col me-3" onChange={e=>setNameSearch(e.target.value)}/>
+          <input type='text' placeholder="Filter using location" className="form-control col" onChange={e=>setLocationSearch(e.target.value)}/>
+          </div>
+        </div>
+        <table className="table table-striped table-hover">
+          <thead className=" table-header">
+            <tr className="tr-head">
+              <th scope="col">ID</th>
+              <th scope="col">NAME</th>
+              <th scope="col">LOCATION</th>
+              <th scope="col">SUBLOCATION</th>
+              <th scope="col">ADDRESS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredSchoolData.map((school) => (
+            <tr className="table-data-row" key={school.id}>
+              <th scope="row">{school.id}</th>
               <td>{school.name}</td>
               <td>{school.location}</td>
               <td>{school.sub_location}</td>
               <td>{school.address}</td>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      </>
   );
 };
 
